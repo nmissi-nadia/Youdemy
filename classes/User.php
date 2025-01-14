cf36d651cf43480b051b9aef38fe8b227f202384
nmissi-nadia <nmissinadia@gmail.com>
2025-01-15 00:52:21 +0100

    Class user

--- classes/User.php
+++ classes/User.php
@@ -1,37 +1,73 @@
-<?php 
-require_once 'db.php';
-require_once 'session.php';
-abstract class user{
-    protected $id;
-    protected $nom;
-    protected $prenom;
-    protected $email;
-    protected $password;
-    protected $role;
-    function __construct($nom,$prenom,$email,$role,$id=null,$password=null)
-    {
-        $this->id=$id;
-        $this->nom=$nom;
-        $this->prenom=$prenom;
-        $this->email=$email;
-        $this->role=$role;
-        $this->password=$password;
-    }
-    private function setPassword($password){
-     $this->password=password_hash($password,PASSWORD_DEFAULT);
-    // $this->password=$password;  
-    }
-    public function __get($attribut) {
-        
-        if (property_exists($this, $attribut)) {
-            return $this->$attribut;
-        } else {
-           
-            echo "L'attribut '$attribut' n'existe pas.";
-        }
+<?php
+require_once __DIR__ . './../config/db_config.php';
+
+class User {
+    protected int $id;
+    protected string $nom;
+    protected string $prenom;
+    protected string $telephone;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+    protected string $photo;
+    protected bool $status;
+    protected $database;
+
+    public function __construct() {
+        $this->database = Database::getInstance()->getConnection();
     }
 
+    // GETTERS
+    public function getId(): int {
+        return $this->id;
+    }
+    public function getNom(): string {
+        return $this->nom;
+    }
+    public function getPrenom(): string {
+        return $this->prenom;
+    }
+    public function getTelephone(): string {
+        return $this->telephone;
+    }
+    public function getEmail(): string {
+        return $this->email;
+    }
+    public function getPassword(): string {
+        return $this->password;
+    }
+    public function getRole(): string {
+        return $this->role;
+    }
+    public function getPhoto(): string {
+        return $this->photo;
+    }
+    public function getStatus(): bool {
+        return $this->status;
+    }
 
+    // SETTERS
+    public function setNom(string $nom): void {
+        $this->nom = $nom;
+    }
+    public function setPrenom(string $prenom): void {
+        $this->prenom = $prenom;
+    }
+    public function setTelephone(string $telephone): void {
+        $this->telephone = $telephone;
+    }
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+    public function setPassword(string $password): void {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }
+    public function setRole(string $role): void {
+        $this->role = $role;
+    }
+    public function setStatus(string $status): void {
+        $this->status = $status;
+    }
     abstract static public function login($email, $password);
 
     private function  insertion(){
