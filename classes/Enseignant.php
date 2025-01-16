13329c92123582e73f75c163e8158b5a58191597
nmissi-nadia <nmissinadia@gmail.com>
2025-01-16 14:02:07 +0100

    authentification

--- classes/Enseignant.php
+++ classes/Enseignant.php
@@ -1,10 +1,29 @@
 <?php
-
-    require_once __DIR__ .'./User.php';
+    require_once 'User.php';
+    require_once 'Database.php';
+    
 
     class Enseignant extends User {
 
-        
+        public static function login($email, $password) {
+            $db = Database::getInstance();
+            $pdo = $db->getConnection();
+    
+            $stmt = $pdo->prepare("SELECT * FROM user WHERE email = ? AND role = 'Enseignant'");
+            $stmt->execute([$email]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+            if ($user && password_verify($password, $user['password'])) {
+                $enseignant = new self();
+                $enseignant->setPrenom($user['prenom']);
+                $enseignant->setEmail($user['email']);
+                $enseignant->setRole($user['role']);
+                $enseignant->setStatus($user['status']);
+                return $enseignant;
+            } else {
+                return null;
+            }
+        }
 
     }
 
