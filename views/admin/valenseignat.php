aa10861027cfd8054d49c19ba11f56de85633147
nmissi-nadia <nmissinadia@gmail.com>
2025-01-23 11:14:26 +0100

    ajout button de déconnexion

--- views/admin/valenseignat.php
+++ views/admin/valenseignat.php
@@ -58,6 +58,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['iduser'])) {
                         </svg>
                         <span>Cours</span>
                     </a>
+                    <a href="../logout.php" class="flex items-center space-x-3 p-3 rounded-lg hover:bg-blue-700">
+                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                        </svg>
+                        <span>Déconnexion</span>
+                    </a>
                 </nav>
             </div>
         </div>
